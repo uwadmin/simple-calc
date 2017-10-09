d5e3228fb13fdadd3fdfbd671f3fe3629604486b
Chao Ma <ynyounuo@me.com>
2017-10-09 15:39:39 -0700

    Reformat

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -20,7 +20,9 @@ extension Double {
 extension String {
     var condensedWhitespace: String {
         let components = self.components(separatedBy: .whitespacesAndNewlines)
-        return components.filter { !$0.isEmpty }.joined(separator: " ")
+        return components.filter {
+            !$0.isEmpty
+        }.joined(separator: " ")
     }
 }
 
